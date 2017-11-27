a6b891f709249408321906530a27df86c7489411
Andrey <luzkovskyandrey@gmail.com>
2017-11-27 20:01:58 +0300

    test api call

--- app/src/containers/Assets/index.tsx
+++ app/src/containers/Assets/index.tsx
@@ -7,19 +7,41 @@ const {
   http
 } = Injector.of();
 
-class Assets extends React.Component {
+class Assets extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
   componentDidMount() {
     //https://raw.githubusercontent.com/cyberFund/chaingear/gh-pages/chaingear.json
     http.GET('/api/tokens')
       .then(data => {
-        console.log(data)
+        this.setState({
+          items: data
+        })
       })
     
   }
   render() {
+    const rows = this.state.items.map(item => (
+      <tr>
+        <td>
+          <img src={item.icon}/>
+        </td>
+        <td>
+          {item.system}
+        </td>
+      </tr>
+    ))
     return (
       <App>
-       Assets
+         <table>
+           <tbody>
+             {rows}
+           </tbody>
+         </table>
       </App>
     );    
   }
