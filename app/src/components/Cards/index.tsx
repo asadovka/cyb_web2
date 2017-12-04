162d0abd981e0efac2b546dacd529e262b827a19
Andrey <luzkovskyandrey@gmail.com>
2017-12-04 19:02:17 +0300

    #22 change card

--- app/src/components/Cards/index.tsx
+++ app/src/components/Cards/index.tsx
@@ -13,21 +13,27 @@ export const CardList = ({ children }) => (
 export const Card = ({ logo, name, descriptions, tokens_sold, system }) => (
   <div className={styles.card}>
     <Link to={`/crowdsales/${system}`}>
-        <div className={styles.imageContainer}>
-          <img width={150} src={logo}/>
-        </div>
-        <h3>{name}</h3>
-        <p>
-          {descriptions}
+    <div className='card'>
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img width={150} src={logo} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div className="card-content">
+         <div className="content">
+           <p className="title is-4">{name}</p>
+           <p className="subtitle is-6">{descriptions}</p>           
+         </div>
+      </div>
+      <footer className="card-footer">
+        <p className="card-footer-item">
+          <span>21 days left</span>
         </p>
-        <div className={styles.dateAndTokens}>
-          <div>
-            21 days left
-          </div>
-          <div>
-            {tokens_sold}
-          </div>
-        </div>
+        <p className="card-footer-item">
+          <span>{tokens_sold}</span>
+        </p>
+      </footer>
+    </div>
     </Link>
   </div>
 )
