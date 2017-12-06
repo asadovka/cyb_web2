7f1cb87b1b300a18e34848ba88aedf1e4dafb764
Anatoli Babenia <anatoli@rainforce.org>
2017-12-06 14:58:08 +0300

    staging: Update `cyber-markets` from its own compose file (#28)

--- devops/staging/up.sh
+++ devops/staging/up.sh
@@ -3,8 +3,17 @@
 set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# directory with repo clones
+HOME="$( dirname $( dirname "$( dirname "$DIR" )" ) )"
+
+git pull
 
 export COMPOSE_FILE="$DIR/docker-compose.yml.staging"
 docker-compose pull
 docker-compose up -d
 
+cd "$HOME/cyber-markets"
+git pull
+export COMPOSE_FILE="devops/staging/docker-compose.yml"
+docker-compose pull
+docker-compose up -d
