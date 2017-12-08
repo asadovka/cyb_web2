57c9bd76eb0dcd1993ea898b6f45ca15c70830dc
Andrey <luzkovskyandrey@gmail.com>
2017-12-08 14:23:23 +0300

    token details

--- app/src/containers/TokensDetails/index.tsx
+++ app/src/containers/TokensDetails/index.tsx
@@ -12,6 +12,38 @@ const {
 
 import { showTokensDetails, getSystemLogoUrl } from '../../modules/chaingear';
 
+
+import {LineChart, Line, AreaChart, Area, Brush, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+const data = [
+      {name: 'Page A', uv: 4000, pv: 9000},
+      {name: 'Page B', uv: 3000, pv: 7222},
+      {name: 'Page C', uv: 2000, pv: 6222},
+      {name: 'Page D', uv: 1223, pv: 5400},
+      {name: 'Page E', uv: 1890, pv: 3200},
+      {name: 'Page F', uv: 2390, pv: 2500},
+      {name: 'Page G', uv: 3490, pv: 1209},
+];
+const PriceChart = React.createClass({
+  render () {
+    return (
+      <div>
+        <h4>A demo of synchronized AreaCharts</h4>
+
+        <LineChart width={600} height={200} data={data} syncId="anyId"
+              margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Line type='monotone' dataKey='pv' stroke='#82ca9d' fill='#82ca9d' />
+          <Brush />
+        </LineChart>
+
+    </div>
+    );
+  }
+});
+
 class TokensDetails extends React.Component<any, any> {
   componentDidMount() {
     const { system } = this.props;
@@ -25,32 +57,31 @@ class TokensDetails extends React.Component<any, any> {
          {crowdsalesDetails.system ? (
            <div>
              <div>
-               <h2>{crowdsalesDetails.system}</h2>
-               <h3>{crowdsalesDetails.token.symbol}</h3>
-               <h3>{crowdsalesDetails.descriptions.state}{' '}{crowdsalesDetails.descriptions.system_type}</h3>
                <img width={50} src={getSystemLogoUrl(crowdsalesDetails, chaingearApi.imageUrl())} />
+               <h2 className='title'>{crowdsalesDetails.system}</h2>
+               <h3 className='subtitle'>{crowdsalesDetails.token.symbol}{' '}{crowdsalesDetails.descriptions.state}{' '}{crowdsalesDetails.descriptions.system_type}</h3>
              </div>
              <div>
                statistic???
              </div>
              <div>
-               <h2>Links:</h2>
+               <h2 className='title'>Links:</h2>
+               <div className='tags'>
                {crowdsalesDetails.links.map(link => (
-                 <div>
-                 <a href={link.url}>
-                   {link.icon && <img width={20} src={chaingearApi.imageUrl() + link.icon} />} {link.name}
+                 <a className='tag' href={link.url}>
+                   {link.icon && <img style={{ marginRight: 10 }} width={20} src={chaingearApi.imageUrl() + link.icon} />} {link.name}
                  </a>
-                 </div>
                 ))}
+                 </div>
              </div>
              <div>
-               price chart ?
+               <PriceChart />
              </div>
              <div>
-               <h2>Specification</h2>
+               <h2 className='title'>Specification</h2>
              </div>
              <div>
-               <h2>First Price</h2>
+               <h2 className='title'>First Price</h2>
              </div>
              <div>
                You can improve <a href={`https://github.com/cyberFund/chaingear/blob/gh-pages/sources/${crowdsalesDetails.system}/${crowdsalesDetails.system}.toml`}>{crowdsalesDetails.system}'s</a> page on Github.
