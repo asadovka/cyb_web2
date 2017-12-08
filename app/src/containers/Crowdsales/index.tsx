4452a7b887053900eeb3c70be524a7e4b6ab6211
Andrey <luzkovskyandrey@gmail.com>
2017-12-08 14:13:17 +0300

    use crowdsales

--- app/src/containers/Crowdsales/index.tsx
+++ app/src/containers/Crowdsales/index.tsx
@@ -16,6 +16,11 @@ import { connect } from 'react-redux';
 
 import { showAllCrowdsales } from '../../modules/chaingear';
 
+import { Logo } from '../../components/AssetTable/';
+var numeral = require('numeral');
+import * as moment from 'moment'
+
+
 class Crowdsales extends React.Component<any, any> {
 
   componentDidMount() {
@@ -27,21 +32,53 @@ class Crowdsales extends React.Component<any, any> {
       items
     } = this.props;
 
-    const cards = items.map(item => (
-      <Card
-        key={item.system}
-        logo={getSystemLogoUrl(item, chaingearApi.imageUrl())}
-        name={item.system}
-        system={item.system}
-        descriptions={item.descriptions && item.descriptions.headline}
-        tokens_sold={item.crowdsales.tokens_sold}
-      />
+    // const cards = items.map(item => (
+    //   <Card
+    //     key={item.system}
+    //     logo={getSystemLogoUrl(item, chaingearApi.imageUrl())}
+    //     name={item.system}
+    //     system={item.system}
+    //     descriptions={item.descriptions && item.descriptions.headline}
+    //     tokens_sold={item.crowdsales.tokens_sold}
+    //   />
+    // ))
+    // <CardList>
+    //        {cards}
+    //      </CardList>
+    const rows = items.map(item => (
+      <tr>
+        <td>
+          <Logo to={`/crowdsales/${item.system}`}>
+            <img width={50} src={getSystemLogoUrl(item, chaingearApi.imageUrl())}/>            
+            <span>{item.system}</span>
+          </Logo>
+        </td>
+        <td>
+          {item.descriptions && item.descriptions.headline}
+        </td>
+        <td>
+          {moment(item.crowdsales.end_date).format('YYYY.MM.DD')}
+        </td>
+        <td>
+          {numeral(item.crowdsales.tokens_sold).format('0,0,0.00')}
+        </td>
+      </tr>
     ))
     return (
       <App>
-         <CardList>
-           {cards}
-         </CardList>
+         <table className='table is-striped is-fullwidth'>
+           <thead>
+             <tr>
+               <th>system</th>
+               <th>descriptions</th>
+               <th>crowdsales</th>
+               <th>tokens sold</th>
+             </tr>
+           </thead>
+           <tbody>
+             {rows}
+           </tbody>
+         </table>
       </App>
     );    
   }
