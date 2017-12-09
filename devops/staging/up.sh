122a423e0178a1239c1599546d198f530ede18c1
Hleb Albau <hleb.albau@gmail.com>
2017-12-09 15:11:08 +0300

    Move chain to staging

--- devops/staging/up.sh
+++ devops/staging/up.sh
@@ -6,10 +6,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git pull
 
-export COMPOSE_FILE="$DIR/staging.common.docker-compose.yml"
-docker-compose pull
+export COMPOSE_FILE="$DIR/staging.chains.docker-compose.yml"
 docker-compose up -d
-sleep 30
+sleep 10
+
+export COMPOSE_FILE="$DIR/staging.common.docker-compose.yml"
+docker-compose up -d
+sleep 10
 
 export COMPOSE_FILE="$DIR/staging.search.docker-compose.yml"
 docker-compose pull
