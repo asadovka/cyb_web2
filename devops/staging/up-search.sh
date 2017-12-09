a69bfaf7b1863c4fde97c58b0f810fca3d269871
Hleb Albau <hleb.albau@gmail.com>
2017-12-09 14:39:39 +0300

    Separate staging by services

--- devops/staging/up-search.sh
+++ devops/staging/up-search.sh
@@ -6,16 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git pull
 
-export COMPOSE_FILE="$DIR/staging.common.docker-compose.yml"
-docker-compose pull
-docker-compose up -d
-sleep 30
-
 export COMPOSE_FILE="$DIR/staging.search.docker-compose.yml"
-docker-compose pull
-docker-compose up -d
-sleep 10
-
-export COMPOSE_FILE="$DIR/staging.markets.docker-compose.yml"
 docker-compose pull
 docker-compose up -d
