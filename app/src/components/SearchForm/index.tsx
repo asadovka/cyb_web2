de15245f3cc41af4c6c7cd31a5a29583c55a16dd
Andrey <luzkovskyandrey@gmail.com>
2018-01-22 20:30:08 +0300

    add serch active colors and animation

--- app/src/components/SearchForm/index.tsx
+++ app/src/components/SearchForm/index.tsx
@@ -1,27 +1,61 @@
 import * as React from "react";
 
-const styles = require("./SearchForm.less");
+const stylesCss = require("./SearchForm.less");
 import { browserHistory } from 'react-router'
 import SearchIcon from 'material-ui-icons/Search';
+import { withStyles, MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import green from 'material-ui/colors/green';
 
+import Input, { InputLabel } from 'material-ui/Input';
+import purple from 'material-ui/colors/purple';
+import red from 'material-ui/colors/red';
+const styles = theme => ({
+})
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiInput: {
+      inkbar: {
+        '&:after': {
+          backgroundColor: '#00ff00'
+        }
+      },
+      underline: {
+        color: '#fff',
+        '&:before': {
+          backgroundColor: 'rgba(255, 255, 255, 0.7)'
+        },
+        '&:hover:not($disabled):before': {
+          backgroundColor: '#fff',
+        },
+      },
+    },
+  },
+});
 
-function SearchForm(props) {
+let input; //TODO: fix
+function SearchForm({ classes }) {
   const onSubmit = (e) => {
     e.preventDefault();
     browserHistory.push('/search?q=' + input.value);
   }
-  let input;
   return (
+    <MuiThemeProvider theme={theme}>
     <form onSubmit={onSubmit}>
-      <div className={styles.wrapper}>
-        <div className={styles.search}>
+      <div className={stylesCss.wrapper}>
+        <div className={stylesCss.search}>
           <SearchIcon />
         </div>
-        <input ref={node => input = node } id="docsearch-input" className={styles.input} />
+        <Input 
+          inputRef={node => {
+            input = node 
+          }} 
+          fullWidth={true}         
+        />
       </div>
     </form>
+    </MuiThemeProvider>
   );
 }
 
-export default SearchForm;
+export default withStyles(styles)(SearchForm);
