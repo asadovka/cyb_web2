c58c98f8c36266b4fa3e89f176fd14c26338171a
Andrey <luzkovskyandrey@gmail.com>
2018-05-14 17:07:07 +0300

    eth pages

--- stories/ethereum.stories.js
+++ stories/ethereum.stories.js
@@ -32,17 +32,21 @@ import Tabs, { Tab } from '../app/src/components/Tabs/';
 
 import Container from '../app/src/components/Container/';
 
-storiesOf('Ethereum pages', module)
-.add('block', withState({ open: false })(({ store }) => (
+
+import QRCode from '../app/src/components/QRCode';
+import Robohash from '../app/src/components/Robohash';
+
+
+const App = ({ onToggle, open, children }) => (
   <Layout 
-    open={store.state.open}
-    onToggle={() => store.set({ open: !store.state.open })}
+    open={open}
+    onToggle={onToggle}
   >
     <AppHeader>
       <SearchForm />
     </AppHeader>
     <AppMenu>
-      <Menu open={store.state.open} >
+      <Menu open={open} >
         <MenuItem icon='stratis' to='/tokens'>Tokens</MenuItem>
         <MenuItem icon='mail' to='/contracts'>Contracts</MenuItem>
         <MenuItem icon='internet' to='/labels'>Labels</MenuItem>
@@ -55,7 +59,19 @@ storiesOf('Ethereum pages', module)
       </Menu>
     </AppMenu>
     <AppContent >
-            <Container>
+      <Container>
+        {children}
+      </Container>
+    </AppContent>
+  </Layout>
+)
+
+storiesOf('Ethereum pages', module)
+.add('block', withState({ open: false })(({ store }) => (
+  <App 
+    open={store.state.open}
+    onToggle={() => store.set({ open: !store.state.open })}
+  >
         <Head>
           <Button o>previous</Button>
           <Title inline={true}>Ethereum Block #5000000</Title>
@@ -161,12 +177,202 @@ storiesOf('Ethereum pages', module)
               </tr>
           </thead>
         </TransactionsTable>
-      </Container>
-    </AppContent>
-  </Layout>
-)));
+    </App>
+)))
+.add('transaction', withState({ open: false })(({ store }) => (
+  <App 
+    open={store.state.open}
+    onToggle={() => store.set({ open: !store.state.open })}
+  >
+        <div style={{ textAlign: 'center' }}>
+          <Title inline={true}>Ethereum Transaction</Title>
+        </div>
+        <SubTitle>Overview</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>time(UTC)</Label>
+            <Value>7/2/2018 17:13:33 UTC</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>transaction hash</Label>
+            <Value>0xb4de9f39cf7b6218d51ded0174007d4f9344ddfa690f9c94af00b4d18b7d3bb0</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>block</Label>
+            <Value>5000000</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>nonce</Label>
+            <Value>3</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>status</Label>
+            <Value>Confirmed (???, ??? confirmations)</Value>
+          </DetailsRow>
+        </Details>
 
+        <SubTitle>Details</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>Type</Label>
+            <Value>???</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>from</Label>
+            <Value>0xb4de9f39cf7b6218d51ded0174007d4f9344ddfa690f9c94af00b4d18b7d3bb0</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>to</Label>
+            <Value>0xb4de9f39cf7b6218d51ded0174007d4f9344ddfa690f9c94af00b4d18b7d3bb0</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>value</Label>
+            <Value>3 УЕР</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>fee</Label>
+            <Value>0ю0002 УЕР</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>gas price</Label>
+            <Value>3 GWEI</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>gas used</Label>
+            <Value></Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>gas limit</Label>
+            <Value>21000</Value>
+          </DetailsRow>
+        </Details>
 
+        <SubTitle>Data</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>input</Label>
+            <Value>???</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>logs</Label>
+            <Value>???</Value>
+          </DetailsRow>
+        </Details>
+  </App>
+)))
+.add('uncle', withState({ open: false })(({ store }) => (
+  <App 
+    open={store.state.open}
+    onToggle={() => store.set({ open: !store.state.open })}
+  >
+    <div>
+        <Head>
+          <Title inline={true}>Ethereum Uncle Block #500000</Title>
+        </Head>
+        <SubTitle>Overview</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>Time (UTC)</Label>
+            <Value>2018-01-30T17:07:12+03:00</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>parent block height</Label>
+            <Value>500001</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>parent block hash</Label>
+            <Value>0x0a264d62c3b7f50ba3464649fde3f2f3e1c776d80b335263c0923899a2d7f09e</Value>
+          </DetailsRow>
+          <DetailsRow>
+            <Label>uncle level</Label>
+            <Value>1</Value>
+          </DetailsRow>
+        </Details>
 
+        <SubTitle>Mining</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>miner</Label>
+            <Value>0x84990f5d2e09f56cabdabf6409ad31bdd8363b50</Value>
+          </DetailsRow>
+        </Details>
+
+        <SubTitle>Rewards</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>uncle inclusion reward</Label>
+            <Value>2.625 ETH</Value>
+          </DetailsRow>
+        </Details>
+      </div>
+  </App>
+)))
+.add('contract', withState({ open: false })(({ store }) => (
+  <App 
+    open={store.state.open}
+    onToggle={() => store.set({ open: !store.state.open })}
+  >
+      <div>
+        <Title>Ethereum Contract</Title>
+        <Head>
+          <div>
+            <Robohash hash='0xa7f995d07929ceb231b27523e3feb7478203b254ff6d3b7e27cf1e8383022dad' />
+          </div>
+          <div>
+            <QRCode hash='0xa7f995d07929ceb231b27523e3feb7478203b254ff6d3b7e27cf1e8383022dad' />
+          </div>
+          <div>
+            <div style={{
+              width: 55,
+              height: 55,
+              background: '#438cef',
+              borderRadius: '50%',
+              boxShadow: '0px 1px 4px #438cef'
+            }}>
+            </div>
+          </div>
+        </Head>
+        <SubTitle>Overview</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>Time (UTC)</Label>
+            <Value>07.02.2018 17:13:33 (UTC)</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>Last activity</Label>
+            <Value>3 min ago</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>address hash</Label>
+            <Value>1C1mCxRukix1KfegAY5zQQJV7samAciZpv</Value>        
+          </DetailsRow>
+        </Details>
+
+        <SubTitle>Balance</SubTitle>
+        <Details>
+          <DetailsRow>
+            <Label>Transactions</Label>
+            <Value>12</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>Unconfirmed transactions</Label>
+            <Value>???</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>Lifetime balance</Label>
+            <Value>???</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>Current balance</Label>
+            <Value>5 ETH</Value>        
+          </DetailsRow>
+          <DetailsRow>
+            <Label>Current balance with unconfirmed transactions</Label>
+            <Value>???</Value>        
+          </DetailsRow>
+        </Details>
+      </div>
+  </App>
+)))
 
   
